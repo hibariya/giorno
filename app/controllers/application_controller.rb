cf1082cd6ebb972dc0945446f1c678ac1b9e4a3c
hibariya <celluloid.key@gmail.com>
2011-08-17 23:19:28 +0900

    cheap auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def signed_in?
+    session[:uid].present?
+  end
+
+  helper_method :signed_in?
+
+  def signin_required
+    redirect_to signin_path unless signed_in?
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,24 @@
 class SessionsController < ApplicationController
+  def new
+    redirect_to '/auth/open_id'
+  end
+
   def create
+    auth = request.env["omniauth.auth"].symbolize_keys
+
+    if auth[:uid] == Settings.open_id.uid
+      session[:uid] = auth[:uid]
+
+      redirect_to admin_entries_path
+    end
   end
 
   def destroy
+    reset_session
+
+    redirect_to root_path
   end
 
   def failure
   end
-
 end
